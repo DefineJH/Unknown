452362113338f68f7c3167527e19d8598a2770f3
anhaeyeong <91206465+anhaeyeong@users.noreply.github.com>
2021-11-08 18:09:00 +0900

    stamina
    asd

---
+++ Source/Unknown/AI_AnimInstance.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI_AnimInstance.h"
+#include "Animation/AnimMontage.h"
+
+UAI_AnimInstance::UAI_AnimInstance()
+{
+	bIsAttack = false;
+	ConstructorHelpers::FObjectFinder<UAnimMontage> montage(TEXT("AnimMontage'/Game/AI/Bear/AttackMontage.AttackMontage'"));
+	if (montage.Succeeded())
+	{
+		AttackMontage = montage.Object;
+	}
+}
+
+void UAI_AnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+}
+
+void UAI_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+}
+
+void UAI_AnimInstance::Play_Attack_Anim_Implementation()
+{
+	if (AttackMontage)
+	{
+		if (!Montage_IsPlaying(AttackMontage))
+		{
+			Montage_Play(AttackMontage);
+		}
+	}
+}
+
+void UAI_AnimInstance::Play_Dead_Anim_Implementation()
+{
+	if (DieMontage)
+	{
+		if (!Montage_IsPlaying(DieMontage))
+		{
+			Montage_Play(DieMontage);
+		}
+	}
+}
+
---
+++ Source/Unknown/AI_AnimInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AI_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+
+class UAnimMontage;
+
+UCLASS()
+class UNKNOWN_API UAI_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UAI_AnimInstance();
+
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UFUNCTION(BlueprintNativeEvent)
+		void Play_Attack_Anim();
+	UFUNCTION(BlueprintNativeEvent)
+	void Play_Dead_Anim();
+public:
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite)
+		UAnimMontage* AttackMontage;
+	UPROPERTY(EditDefaultsOnly)
+		UAnimMontage* DieMontage;
+private:
+	bool bIsAttack;
+};
